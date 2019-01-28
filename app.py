2e4a323a79061594f5febaebbadbc76c65b0680d
aaaa <amack832@gmail.com>
2019-01-28 14:03:47 -0600

    uploading app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+db = SQLAlchemy(app)
+
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
+
+@app.route("/")
+def index():
+
+    return redner_template("index.html")
+
+@app.route("")

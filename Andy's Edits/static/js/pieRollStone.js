637a2aa672d7b7295f870d4381be0c9ef858574f
ByronIAllen <ByronIAllen@users.noreply.github.com>
2019-02-08 18:15:46 -0600

    switched to stacked bar

--- Andy's Edits/static/js/pieRollStone.js
+++ Andy's Edits/static/js/pieRollStone.js
@@ -1,165 +1,123 @@
-var defaultURL = '/pitchData';
+// Set route
+var defaultURL = '/pitchtotals';
+
+// create lists for each genre
+var rockCounts = [];
+var metalCounts = [];
+var popCounts = [];
+var rapCounts = [];
+var electroCounts = [];
+var jazzCounts = [];
+var folkCounts = [];
+var globalCounts = [];
+
+// Create list of years
+var years = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012];
 
 function buildPlot() {
-    // create lists for each genre
-    var rockYears = [];
-    var rockScores = [];
-    var metalYears = [];
-    var metalScores = [];
-    var popYears = [];
-    var popScores = [];
-    var rapYears = [];
-    var rapScores = [];
-    var electroYears = [];
-    var electroScores = [];
-    var jazzYears = [];
-    var jazzScores = [];
-    var folkYears = [];
-    var folkScores = [];
-    var globalYears = [];
-    var globalScores = [];
 
     d3.json(defaultURL).then(function(data) {
-        // Loop though data and place into proper list
-        for (var i=0 ; i < data.length ; i++) {
-            if (data.genre == "rock") {
-                rockYears.push(data[i].year);
-                rockScores.push(data[i].score);
-            } else if (data.genre == "metal") {
-                metalYears.push(data[i].year);
-                metalScores.push(data[i].score);
-            } else if (data.genre == "pop/r&b") {
-                popYears.push(data[i].year);
-                popScores.push(data[i].score);
-            } else if (data.genre == "rap") {
-                rapYears.push(data[i].year);
-                rapScores.push(data[i].score);
-            } else if (data.genre == "electronic") {
-                electroYears.push(data[i].year);
-                electroScores.push(data[i].score);
-            } else if (data.genre == "jazz") {
-                jazzYears.push(data[i].year);
-                jazzScores.push(data[i].score);
-            } else if (data.genre == "folk/country") {
-                folkYears.push(data[i].year);
-                folkScores.push(data[i].score);
-            } else if (data.genre == "global") {
-                globalYears.push(data[i].year);
-                globalScores.push(data[i].score);
+        // Loop through years to make sure data is stored in proper order
+        for (var j=1999 ; j < 2013 ; j++){
+            // Loop though data
+            for (var i=0 ; i < data.length ; i++) {
+                // Place data into proper genre's list
+                if (data[i].genre == "rock" && data[i].year == j) {
+                    rockCounts.push(data[i].genre_count);
+                } else if (data[i].genre == "metal" && data[i].year == j) {
+                    metalCounts.push(data[i].genre_count);
+                } else if (data[i].genre == "pop/r&b" && data[i].year == j) {
+                    popCounts.push(data[i].genre_count);
+                } else if (data[i].genre == "rap" && data[i].year == j) {
+                    rapCounts.push(data[i].genre_count);
+                } else if (data[i].genre == "electronic" && data[i].year == j) {
+                    electroCounts.push(data[i].genre_count);
+                } else if (data[i].genre == "jazz" && data[i].year == j) {
+                    jazzCounts.push(data[i].genre_count);
+                } else if (data[i].genre == "folk/country" && data[i].year == j) {
+                    folkCounts.push(data[i].genre_count);
+                } else if (data[i].genre == "global" && data[i].year == j) {
+                    globalCounts.push(data[i].genre_count);
+                }
             }
         };
 
         // Load lists into traces
         var trace1 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: rockYears,
-            y: rockScores,
-            marker: {
-                size: 5,
-                color: blue,
-                opacity: 0.3
-            }
+            x: years,
+            y: rockCounts,
+            name: "Rock",
+            type: "bar"
         };
 
         var trace2 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: metalYears,
-            y: metalScores,
-            marker: {
-                size: 5,
-                color: red,
-                opacity: 0.3
-            }
+            x: years,
+            y: metalCounts,
+            name: "Metal",
+            type: "bar"
         };
 
         var trace3 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: popYears,
-            y: popScores,
-            marker: {
-                size: 5,
-                color: cyan,
-                opacity: 0.3
-            }
+            x: years,
+            y: popCounts,
+            name: "Pop/R&B",
+            type: "bar"
         };
 
         var trace4 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: rapYears,
-            y: rapScores,
-            marker: {
-                size: 5,
-                color: green,
-                opacity: 0.3
-            }
+            x: years,
+            y: rapCounts,
+            name: "Rap",
+            type: "bar"
         };
 
         var trace5 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: electroYears,
-            y: electroScores,
-            marker: {
-                size: 5,
-                color: orange,
-                opacity: 0.3
-            }
+            x: years,
+            y: electroCounts,
+            name: "Electronic",
+            type: "bar"
         };
 
         var trace6 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: jazzYears,
-            y: jazzScores,
-            marker: {
-                size: 5,
-                color: purple,
-                opacity: 0.3
-            }
+            x: years,
+            y: jazzCounts,
+            name: "Jazz",
+            type: "bar"
         };
 
         var trace7 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: folkYears,
-            y: folkScores,
-            marker: {
-                size: 5,
-                color: grey,
-                opacity: 0.3
-            }
+            x: years,
+            y: folkCounts,
+            name: "Folk/Country",
+            type: "bar"
         };
 
         var trace8 = {
-            type: 'scatter',
-            mode: 'markers',
-            x: globalYears,
-            y: globalScores,
-            marker: {
-                size: 5,
-                color: pink,
-                opacity: 0.3
-            }
+            x: years,
+            y: globalCounts,
+            name: "Global",
+            type: "bar"
         };
 
+        // Set layout for graph
         var layout = {
-            title: "Visualization of Pitchfork Data",
+            barmode: 'stack',
+            title: "Number of Albums Reviewed by Genre Per Year",
             xaxis: {
                 title: "Years"
             },
             yaxis: {
-                title: "Score"
+                title: "# of Reviews"
             },
-            legend = true
-        }
+            legend: true
+        };
         
+        // Compile traces into single list
         var data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7, trace8]
 
-        Plotly.plot('scatter', data, layout);
+        // Plot that bad boy
+        Plotly.plot('bar-stack', data, layout);
     })
 }
 buildPlot();
+console.log(globalCounts);
